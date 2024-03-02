6bfc540423a4e27cb354bf035756ebc053781c0d
Pranav Suryadevara <suryadevarapranav28@gmail.com>
2024-03-02 03:30:00 -0600

    Added Rock Paper Scissors

--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,9 @@
-Just Playing around stuff at Rice and pushing things here that aren't worth a repo by themselves.
+### Repository Description
+
+Just goofing around with stuff here at Rice. Pushing things that aren't worth a repo by themselves but give a slight fun I guess :). Random codes, be it simple, complex :(. Womp Womp Womp......
+
+##### Programs:
+
+Playing Around With Spotify API - February 7th.
+Simple Rock Paper Scissors Game - March 3rd.
+
---
+++ Rock Paper Scissors/RPS.py
@@ -0,0 +1,43 @@
+import random
+
+def get_choices():
+  player_choice = input("Enter a choice (rock, paper, scissors): ")
+
+  options = ["rock", "paper", "scissors"]
+  computer_choice = random.choice(options)
+
+  choices = {"player": player_choice, "computer": computer_choice}
+
+  return choices
+
+
+def check_win(player, computer):
+    print(f"You chose {player}, computer chose {computer}")
+
+    if player == computer:
+        return "It's a tie!"
+    
+    elif player == "rock":
+        if computer == "paper":
+            return "Computer Wins! Paper covers rock"
+        else:
+            return "Player Wins! Rock smashes scissors"
+
+    elif player == "paper":
+        if computer == "rock":
+            return "Player Wins! Paper covers rock"
+        else:
+            return "Computer Wins! Scissors cuts paper"
+    
+    else:
+        if computer == "rock":
+            return "Computer Wins! Rock smashes scissors"
+        else:
+            return "Player Wins! Scissors cuts paper"
+
+
+choices = get_choices()
+
+result = check_win(choices["player"], choices["computer"])
+
+print(result)
